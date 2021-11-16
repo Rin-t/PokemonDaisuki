c13eac5a5f649866e2c78501a4025813d9637187
Rin <rin.t.4921@gmail.com>
2021-11-16 22:28:54 +0900

    first commit

---
+++ PokemonDaisuki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonDaisuki
+//
+//  Created by Rin on 2021/11/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
