bf2fd291a179b7d317d2d13b720c9f547c90ba86
sauna1137 <subscription.search@gmail.com>
2021-12-23 22:05:30 +0900

    各メソッドをstaticへ　switchRange〜メソッドのcaseで指定するレンジにFetchAPIs()を追加
    APIを取得するメソッドはcompletionで受け取れる

--- PokemonDaisuki/Models/FetchAPIs.swift
+++ PokemonDaisuki/Models/FetchAPIs.swift
@@ -22,9 +22,9 @@ struct FetchAPIs {
 
     // MARK: - それぞれの画面で呼び出すメソッド
     // ポケモン151匹のJSONデータを取得
-    func decodePokemonData(typeOfFetch: TypeOfFetch) {
+   static func decodePokemonData(completion: @escaping ([Pokemon]) -> Void) {
 
-        fetchData(typeOfFetch: .pokemon) { dataArray in
+         FetchAPIs.fetchData(typeOfFetch: .pokemon) { dataArray in
             var jsons: [Pokemon] = []
 
             for data in dataArray {
@@ -35,13 +35,14 @@ struct FetchAPIs {
                     print(err)
                 }
             }
+             completion(jsons)
         }
     }
 
     // アイテムカテゴリーのJSONデータを取得
-    func decodeItemCategoriesData(typeOfFetch: TypeOfFetch) {
+   static func decodeItemCategoriesData(completion: @escaping ([ItemCategories]) -> Void) {
 
-        fetchData(typeOfFetch: .itemCategories) { dataArray in
+         FetchAPIs.fetchData(typeOfFetch: .itemCategories) { dataArray in
             var jsons: [ItemCategories] = []
 
             for data in dataArray {
@@ -52,13 +53,14 @@ struct FetchAPIs {
                     print(err)
                 }
             }
+             completion(jsons)
         }
     }
 
     // 地域ごとのJSONデータを取得
-    func decodeRegionData(typeOfFetch: TypeOfFetch) {
+   static func decodeRegionData(completion: @escaping ([Region]) -> Void) {
 
-        fetchData(typeOfFetch: .region) { dataArray in
+        FetchAPIs.fetchData(typeOfFetch: .region) { dataArray in
             var jsons: [Region] = []
 
             for data in dataArray {
@@ -69,15 +71,16 @@ struct FetchAPIs {
                     print(err)
                 }
             }
+            completion(jsons)
         }
     }
 
     // MARK: - 上記のそれぞれの画面で呼び出すメソッドの中で使用するメソッド群
     // APIのURL群を使用してDataの配列を受け取る
-    private func fetchData(typeOfFetch: TypeOfFetch, completion: @escaping ([Data]) -> Void) {
+    private static func fetchData(typeOfFetch: TypeOfFetch, completion: @escaping ([Data]) -> Void) {
 
         let idRange = switchRangeFromTypeOfFetch(typeOfFetch: typeOfFetch)
-        let stringURLs = switchURLsFromTypeOfFetch(typeOfFetch: typeOfFetch, range: idRange)
+        let stringURLs = FetchAPIs.switchURLsFromTypeOfFetch(typeOfFetch: typeOfFetch, range: idRange)
         var dataArray: [Data] = []
 
         // URLRequest型にstringURLsを変換
@@ -105,23 +108,22 @@ struct FetchAPIs {
             task.resume()
         }
     }
-
     // fetchData()内のidRangeを求めるのに使用する 　配列を出力
-    private func switchRangeFromTypeOfFetch(typeOfFetch: TypeOfFetch) -> ClosedRange<Int> {
+    private static func switchRangeFromTypeOfFetch(typeOfFetch: TypeOfFetch) -> ClosedRange<Int> {
 
         let range: ClosedRange<Int>
 
         switch typeOfFetch {
-        case .itemCategories: range = itemCategoryIDRange
-        case .pokemon: range = pokemonIDRange
-        case .region: range = regionIDRange
+        case .itemCategories: range = FetchAPIs().itemCategoryIDRange
+        case .pokemon: range = FetchAPIs().pokemonIDRange
+        case .region: range = FetchAPIs().regionIDRange
         }
 
         return range
     }
 
     // fetchData()内のstringURLsを求めるのに使用する  URLの配列を出力　一番初めにAPIで取得するURL群
-    private func switchURLsFromTypeOfFetch(typeOfFetch: TypeOfFetch, range: ClosedRange<Int>) -> [String] {
+    private static func switchURLsFromTypeOfFetch(typeOfFetch: TypeOfFetch, range: ClosedRange<Int>) -> [String] {
 
         let urls: [String] = range.map { id in
 
