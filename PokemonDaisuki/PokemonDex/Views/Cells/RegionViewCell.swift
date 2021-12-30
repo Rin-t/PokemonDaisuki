4fff8de3a9ef5fa8faeb6b6f419a1d295cd2b4a2
sauna1137 <subscription.search@gmail.com>
2021-12-30 15:46:35 +0900

    RegionViewCell完成

--- PokemonDaisuki/PokemonDex/Views/Cells/RegionViewCell.swift
+++ PokemonDaisuki/PokemonDex/Views/Cells/RegionViewCell.swift
@@ -9,9 +9,28 @@ import UIKit
 
 class RegionViewCell: UICollectionViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+
+    @IBOutlet private weak var regionButton: UIButton!
+    @IBOutlet private weak var regionLabel: UILabel!
+
+    static let identifier = "RegionViewCell"
+    static func regionViewNib() -> UINib {
+        UINib(nibName: identifier, bundle: nil)
+    }
+
+    private var tappedButton = { () -> Void in }
+
+    func configure(title: String, cellCornerRadius: CGFloat, tappedButton: @escaping () -> Void) {
+        regionLabel.text = title
+        regionLabel.textColor = .black
+        regionButton.backgroundColor = .systemBlue
+        regionButton.tintColor = .white
+        regionButton.layer.cornerRadius = cellCornerRadius
+        self.tappedButton = tappedButton
+    }
+
+    @IBAction func tappedRegionButton(_ sender: Any) {
+        tappedButton()
     }
 
 }
