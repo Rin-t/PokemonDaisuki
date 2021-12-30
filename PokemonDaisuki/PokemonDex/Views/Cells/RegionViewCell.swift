8c0d8817308766ddf828741d7ebff339b5441349
sauna1137 <subscription.search@gmail.com>
2021-12-30 14:19:39 +0900

    RegionView追加 first commit

---
+++ PokemonDaisuki/PokemonDex/Views/Cells/RegionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  RegionViewCell.swift
+//  PokemonDaisuki
+//
+//  Created by KS on 2021/12/30.
+//
+
+import UIKit
+
+class RegionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
