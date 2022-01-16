f76fc4f752de7c87360026eb410441d7da3a7676
sauna1137 <subscription.search@gmail.com>
2022-01-16 17:47:36 +0900

    APIデータを取得するメソッドをviewDidLoadで呼び出しcollectionViewへ地方の情報を表示 そもそも地方表示をAPIから行うか疑問に思ってしまったが勉強、練習のためにこのままにする。

--- PokemonDaisuki/PokemonDex/Controllers/RegionViewController.swift
+++ PokemonDaisuki/PokemonDex/Controllers/RegionViewController.swift
@@ -11,9 +11,17 @@ final class RegionViewController: UIViewController {
 
     @IBOutlet private weak var collectionView: UICollectionView!
 
+    private var regions:[Region] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupCollectionView()
+        FetchAPIs.decodeRegionData { Regions in
+            self.regions = Regions
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
+        }
     }
 
     private func setupCollectionView() {
@@ -32,15 +40,15 @@ final class RegionViewController: UIViewController {
 extension RegionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return regions.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RegionViewCell.identifier, for: indexPath) as! RegionViewCell
 
-        cell.configure(title: "地方", cellCornerRadius: view.frame.width / 5) {
-            print(indexPath)
+        cell.configure(title: regions[indexPath.row].names[0].name, cellCornerRadius: view.frame.width / 5) {
+            print(self.regions[indexPath.row].pokedexes[0].url)
         }
         return cell
     }
