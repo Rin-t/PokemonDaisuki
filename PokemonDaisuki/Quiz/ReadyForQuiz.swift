fe424af6663f5c8d16ec1dcfc04eab25bab92980
0102johnny <rt2824.recruit.1022@gmail.com>
2022-03-06 16:31:20 +0900

    不要なコメントアウトの削除

--- PokemonDaisuki/Quiz/ReadyForQuiz.swift
+++ PokemonDaisuki/Quiz/ReadyForQuiz.swift
@@ -9,19 +9,12 @@ import UIKit
 
 class ReadyForQuiz: UIViewController {
 
-    
     @IBOutlet weak var startQuizButton: UIButton! {
         didSet{
             startQuizButton.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
         }
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
     @objc func didTapButton() {
         let vc = TestQuizViewController()
         let nav = UINavigationController(rootViewController: vc)
@@ -29,15 +22,9 @@ class ReadyForQuiz: UIViewController {
         present(nav, animated: true, completion: nil)
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        // Do any additional setup after loading the view.
     }
-    */
-
 }
